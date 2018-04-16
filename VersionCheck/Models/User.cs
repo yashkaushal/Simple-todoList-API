ae16f0621d4c8a9898e682340def16924611ea6a
Yash Kaushal <ykaushal25@gmail.com>
2018-04-16 17:19:41 +0530

    Added users class

---
+++ VersionCheck/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VersionCheck.Models
+{
+    public class User
+    {
+        public static int NumberOfUsers { get; private set; } = 0;
+        public static List<User> list;
+
+        public int Id { get; private set; }
+        public string Name { get;  set; }
+
+        public User(string name)
+        {
+            this.Id = NumberOfUsers++;
+            this.Name = name;
+            list.Add(this);
+        }
+    }
+}
