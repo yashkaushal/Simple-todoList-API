6b5b1bf3fb7149456f4576868df27d48a8c981f7
Yash Kaushal <ykaushal25@gmail.com>
2018-04-16 15:22:04 +0530

    Add project files.

---
+++ VersionCheck/Controllers/VersionController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VersionCheck.Controllers
+{
+    [Route("api/[controller]")]
+    public class VersionController : Controller
+    {
+
+        //get latest version number
+        // GET: api/<controller>
+        [HttpGet]
+        public string Get()
+        {
+            return Models.Version.Number;
+        }
+
+        
+        //check if your version is latest
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(string id)
+        {
+            return id == Models.Version.Number ? "Latest" : "obsolete";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ VersionCheck/Models/Version.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VersionCheck.Models
+{
+    public class Version
+    {
+        public static string Number
+        {
+            get; private set;
+        } = "0.1";
+    }
+}
